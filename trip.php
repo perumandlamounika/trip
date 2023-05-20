e43c97d6260973eaafe3f02651567381c66f5141
perumandlamounika <131239546+perumandlamounika@users.noreply.github.com>
2023-05-20 17:23:12 +0530

    trip

---
+++ trip.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="trip.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+  </style>
+</head>
+<body>
+<center><a><h1 class="text-danger">HOLYDAY TRIP </h1></a></center>
+    <nav class="navbar navbar-expand-sm navbar-light bg-info sticky-top">
+        <div class="container-fluid">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+                <div class="dropdown">
+                  <li class="nav-item">
+                    
+                  
+                          
+
+<li class="nav-item">
+<a class="nav-link" href="index.php">Admin Form</a> 
+
+</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<li class="nav-item">
+<a class="nav-link" href="student-create.php">login Form</a>
+
+                      
+</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;           
+</div>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</nav>
+      <!-- Carousel -->
+      <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+        <!-- Indicators/dots -->
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
+          <button type="button" data-bs-target="#demo" data-bs-slide-to="4"></button>
+        </div>
+        
+        <!-- The slideshow/carousel -->
+        <div class="carousel-inner">
+          
+          <div class="carousel-item active">
+            <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/01/09/11/bora-bora.jpg?width=1200" alt="Los Angeles" class="d-block w-100" style="height:550px">
+          </div>
+          <div class="carousel-item">
+            <img src="https://i.kinja-img.com/gawker-media/image/upload/t_ku-large/cwyi2kuavlsfdjuto2rr.jpg" alt="Chicago" class="d-block w-100" style="height:550px">
+          </div>
+          <div class="carousel-item">
+            <img src="https://media.easemytrip.com/media/Blog/India/637762882335748573/6377628823357485734Aqaal.jpg" alt="New York" class="d-block w-100" style="height:550px">
+          </div>
+          <div class="carousel-item">
+            <img src="https://static-blog.treebo.com/blog/wp-content/uploads/2018/07/Gokarna-1.jpg" alt="" class="d-block w-100" style="height:550px">
+          </div>
+          <div class="carousel-item">
+            <img src="https://blog.irctctourism.com/wp-content/uploads/2020/03/devotional-tour-packages.jpg"  alt=" " class="d-block w-100" style="height:550px">
+          </div>
+        </div>
+        
+        <!-- Left and right controls/icons -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </button>
+      </div>
+      <center><h1 style="color:chocolate;">Beach trips</h1></center>
+  <div id="Charity School">
+
+  <div class=" container ">
+    <div class="row ">
+  <div class="col-4 ">
+  <div class="card " style="width:200">
+  <img src="https://nighthelper.com/wp-content/uploads/2014/06/best-beaches-ftr.jpg" alt=" " class="card-img-top" height="300" width="300">
+  <div class="card-body">
+  <center><h6 class="card-title"></h6></center>
+  <center><h1>  <p class="card-text"></h1></p>
+</center>
+
+  </div>
+    </div>
+        </div>
+  <div class="col-4">
+  <div class="card " style="width:200">
+  <img src="https://media.istockphoto.com/id/489833698/photo/two-lounge-chairs-under-tent-on-beach.jpg?s=612x612&w=0&k=20&c=df_6Jkb5YreyaAcGVbdcRrTP4dZ-opUrYWtgRC3eKKI=" class="card-img-top" height="300" width="300">
+  <div class="card-body">
+  <center><h6 class="card-title"></h6></center>
+  <center><h1>  <p class="card-text"></h1></p>
+   
+  </center>
+  </div>
+  </div>
+  </div>
+  <div class="col-4">
+    <div class="card " style="width:150">
+    <img src="https://vaya.in/wp-content/uploads/2021/03/10-Essentials-You-Must-Carry-in-Your-Bag-for-a-Beach-Trip-in-Summer.jpg" class="card-img-top" height="300" width="300">
+    <div class="card-body">
+    <center><h6 class="card-title"></h6></center>
+    <center><h1>  <p class="card-text"></h1></p>
+     
+    </center>
+    </div>
+    </div>
+    </div>
+    <div class="col-4">
+        <div class="card " style="width:200">
+        <img src="https://www.choicehotels.com/cms/images/choice-hotels/demand-articles/img_best-beach-vacations-01/img_best-beach-vacations-01.jpg" class="card-img-top" height="300" width="300">
+        <div class="card-body">
+        <center><h6 class="card-title"></h6></center>
+        <center><h1>  <p class="card-text"></h1></p>
+         
+        </center>
+        </div>
+        </div>
+        </div>
+<div class="col-4">
+<div class="card " style="width:200">
+<img src="https://img.theculturetrip.com/wp-content/uploads/2018/05/sunrise_at_puri_beach.jpg" class="card-img-top" height="300" width="300">
+<div class="card-body">
+<center><h6 class="card-title"></h6></center>
+<center><h1>  <p class="card-text"></h1></p>
+             
+</center>
+</div>
+</div>
+</div>
+<div class="col-4">
+<div class="card " style="width:200">
+<img src="https://static.toiimg.com/photo/msid-88166697,width-96,height-65.cms" height="300" width="300">
+<div class="card-body">
+<center><h6 class="card-title"></h6></center>
+<center><h1>  <p class="card-text"></h1></p>
+                 
+</center>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<center><h1 style="color:chocolate;">cultural&history trips</h1></center>
+<div id="Child welfare">
+              
+<div class=" container ">
+<div class="row ">
+<div class="col-4 ">
+<div class="card " style="width:200">
+<img src="https://images.cnbctv18.com/wp-content/uploads/2019/02/Egypt-Kom-Ombo-temple-1019x573.jpg?impolicy=website&width=617&height=264" alt=" " class="card-img-top" height="300" width="300">
+<div class="card-body">
+<center><h6 class="card-title"></h6></center>
+<center><h1>  <p class="card-text"></h1></p>
+</center>
+              
+</div>
+</div>
+</div> 
+<div class="col-4">
+    <div class="card " style="width:200">
+    <img src="https://cdn.pixabay.com/photo/2020/02/02/17/24/travel-4813658_1280.jpg" class="card-img-top" height="300" width="300">
+    <div class="card-body">
+    <center><h6 class="card-title"></h6></center>
+    <center><h1>  <p class="card-text"></h1></p>
+                     
+    </center>
+    </div>
+    </div>
+    </div>
+    <div class="col-4">
+        <div class="card " style="width:200">
+        <img src="https://static.toiimg.com/thumb/msid-91980533,width-400,resizemode-4/91980533.jpg" class="card-img-top" height="300" width="300">
+        <div class="card-body">
+        <center><h6 class="card-title"></h6></center>
+        <center><h1>  <p class="card-text"></h1></p>
+                         
+        </center>
+        </div>
+        </div>
+        </div>
+        <div class="col-4">
+            <div class="card " style="width:200">
+            <img src="https://www.hindustantimes.com/ht-img/img/2023/04/07/550x309/sarang-pande-k3SHcT9zGkE-unsplash_1680842903191_1680842919509_1680842919509.jpg" class="card-img-top" height="300" width="300">
+            <div class="card-body">
+            <center><h6 class="card-title"></h6></center>
+            <center><h1>  <p class="card-text"></h1></p>
+                             
+            </center>
+            </div>
+            </div>
+            </div>
+            <div class="col-4">
+                <div class="card " style="width:200">
+                <img src="https://drprem.com/travel/wp-content/uploads/sites/53/2020/06/Guide-to-cultural-tourism.jpg" class="card-img-top" height="300" width="300">
+                <div class="card-body">
+                <center><h6 class="card-title"></h6></center>
+                <center><h1>  <p class="card-text"></h1></p>
+                                 
+                </center>
+                </div>
+                </div>
+                </div>
+                <div class="col-4">
+<div class="card " style="width:200">
+<img src="https://assets.traveltriangle.com/blog/wp-content/uploads/2016/08/Golden-Temple-Amritsar.jpg" class="card-img-top" height="300" width="300">
+<div class="card-body">
+<center><h6 class="card-title"></h6></center>
+<center><h1>  <p class="card-text"></h1></p>
+                                     
+</center>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<center><h1 style="color:chocolate;">divotional trips</h1></center>
+<div id="Food Donation">
+              
+<div class=" container ">
+<div class="row ">
+<div class="col-4 ">
+<div class="card " style="width:200">
+<img src="https://dynamic.tourtravelworld.com/package-images/photo-big/dir_11/307443/61040.jpg" alt=" " class="card-img-top" height="300" width="300" >
+<div class="card-body">
+<center><h6 class="card-title"></h6></center>
+<center><h1>  <p class="card-text"></h1></p>
+</center>
+              
+</div>
+</div>
+</div> 
+<div class="col-4 ">
+  <div class="card " style="width:200">
+  <img src="https://www.tourmyindia.com/blog//wp-content/uploads/2015/02/Kanchipuram-Temple.jpg" alt=" " class="card-img-top" height="300" width="300" >
+  <div class="card-body">
+  <center><h6 class="card-title"></h6></center>
+  <center><h1>  <p class="card-text"></h1></p>
+  </center>
+                
+  </div>
+  </div>
+  </div> 
+  <div class="col-4 ">
+    <div class="card " style="width:200">
+    <img src="https://www.transntrip.com/img/details/india/mumbai/1.jpg" alt=" " class="card-img-top" height="300" width="300">
+    <div class="card-body">
+    <center><h6 class="card-title"></h6></center>
+    <center><h1>  <p class="card-text"></h1></p>
+    </center>
+                  
+    </div>
+    </div>
+    </div> 
+    <div class="col-4 ">
+      <div class="card " style="width:200">
+      <img src="https://static2.tripoto.com/media/filter/tst/img/335641/TripDocument/1564841898_img_20190721_112029_2.jpg" alt=" " class="card-img-top" height="300" width="300">
+      <div class="card-body">
+      <center><h6 class="card-title"></h6></center>
+      <center><h1>  <p class="card-text"></h1></p>
+      </center>
+                    
+      </div>
+      </div>
+      </div> 
+      <div class="col-4 ">
+        <div class="card " style="width:200">
+        <img src="https://trekalone.com/wp-content/uploads/2021/05/indian-temple-vrindavan-mathura-radha-410x250.jpg" alt=" " class="card-img-top" height="300" width="300">
+        <div class="card-body">
+        <center><h6 class="card-title"></h6></center>
+        <center><h1>  <p class="card-text"></h1></p>
+        </center>
+                      
+        </div>
+        </div>
+        </div> 
+        <div class="col-4 ">
+          <div class="card " style="width:200">
+          <img src="https://dynamic.tourtravelworld.com/package-images/photo-big/dir_16/461490/340319.jpg" alt=" " class="card-img-top" height="300" width="300">
+          <div class="card-body">
+          <center><h6 class="card-title"></h6></center>
+          <center><h1>  <p class="card-text"></h1></p>
+          </center>
+                        
+          </div>
+          </div>
+          </div> 
+          </div>
+<hr>
+         <fieldset> 
+                <!--footer-->
+  <footer id="footer">
+    <div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>holiday trips</h3>
+        <p>
+          Hyderabad<br>
+          Banglore<br>
+          Delhi<br>
+        </p>
+        <strong>Phone:</strong>+0000000000000000<br>
+        <strong>Email:</strong>holiday trip@gmail.com <br>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Usefull Links</h4>
+        <ul>
+          
+          <li><a href="#">Services</a></li>
+          <li><a href="#">admin form</a></li>
+          <li><a href="#">user form</a></li>
+      
+        </ul>
+        </div>
+
+
+    
+       
+
+        
+        
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+                
+        
+    <div class="container py-4">
+    <div class="copright">
+      &copy; Copyright<strong><span>holiday trips</span></strong>.All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed By <a href="#">mounika goud coding</a>
+      </div>
+    </div>
+    
+  </footer>       
+<!--footer-->
+
+
+<a href="#" class="arrow"><i><img src="scroll.jpg" alt="" height="20" width="20"></i></a>
+         
+</fieldset>  
+
+  </body>
+  </html>
+
+
+
+
+
+
+  
+  
+           
+    
+      
+
+  
+      
+      
+  
+
+            
+                                    
+              
+                  
+    
+  
+                    
+
+
+
+                        
+                      
+        
+    
